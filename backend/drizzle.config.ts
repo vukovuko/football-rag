d82c61a8f1b2f4ddc1c3939f2a21fcdc62474be4
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2025-10-13 00:43:39 +0200

    Fix

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from "drizzle-kit";
-import { env } from "./env.ts";
 
 export default {
   schema: "./src/db/schema/**/*.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
